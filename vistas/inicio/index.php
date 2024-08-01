dbdb49f0586352e07ebd2ec724ae067137b25a8d
Froshle14 <kimkilwan135@gmail.com>
2024-07-31 20:16:02 -0600

    Ligerios cambios al inicio

--- vistas/inicio/index.php
+++ vistas/inicio/index.php
@@ -22,18 +22,42 @@
 			
 			<div class="container w-md-75 bg-white p-5 mt-4 mb-4 border-0 rounded">
 				
-
-	<div id="main">
-		<h1>  Inicio </h1>
-	</div>
-	
+								<h3> ¿Por qué los Senderos Interpretativos en la Universidad Veracruzana? </h3>
+					
+					<p class="text-justify"> 
+Los Senderos Interpretativos son espacios que permiten tener un contacto directo con 
+los visitantes para el desarrollo de una temática o un tópico particular y así, transmitir 
+valores sobre los cuales se quiere dar un mensaje. Cada sendero tiene un objetivo 
+específico que se sustenta en el desarrollo de las actividades, centrando la atención en 
+aspectos con los que el visitante pueda interiorizar y que, a la vez, sirva de hilo conductor 
+de los contenidos del mensaje.
+</p>
+<p class="text-justify"> 
+Para la Universidad Veracruzana los Senderos Interpretativos son espacios de escenarios 
+reales de aprendizaje para los estudiantes. Estos espacios se han destinado a la 
+construcción de proyectos ambientales con enfoques sustentables, siendo una fuente 
+importante de formación de recursos humanos mediante el desarrollo de temas de tesis 
+de grado de licenciatura y posgrado.
+</p>
+<p class="text-justify"> 
+Actualmente, la Universidad ha implementado 3 senderos interpretativos en sus distintas
+facultadas distribuido en las regiones de Orizaba-Córdoba y Xalapa. El primer sendero 
+desarrollado en el 2018 en el Centro Universitario para las Artes, Ciencias y Cultura, 
+Córdoba, el segundo en el 2022 en la Universidad Veracruzana Intercultural (UVI), Sede 
+Grandes Montañas y el ultimo en el 2023 en la Unidad de Ciencias de la Salud – Xalapa 
+(UCS-X). Nuestros senderos interpretativos comparten una misma visión, la gestión
+integral del agua como objetivo principal para el dialogo y la reflexión, pero cada uno 
+abordado desde un tópico diferente como son: la importancia de los entornos naturales;
+la relación del hombre, agua y cambio climático; la relación del hombre, agua y salud.
+Descubre, aprende y divierte en nuestros corridos virtuales de nuestros senderos
+					
+					</p>
 
 			</div>
 			
 			<?php  require 'vistas/footer.php' ?>
 		
 		</div>
-		
 
 	</div> 
 
